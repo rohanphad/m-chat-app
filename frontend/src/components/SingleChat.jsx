bdcd82ebf78e620eb04aedc882c18918b373fd52
Rohan Phad <rohanphad001@gmail.com>
2024-01-25 21:05:33 +0530

    ENDPOINT updated

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -20,7 +20,7 @@ import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from "../Animations/typing.json";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://m-chat-app.onrender.com/";
 let socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
